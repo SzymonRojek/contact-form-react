a6760b3516c79456348d007be3d411cb16f09887
Szymon Rojek <sz.rojek@gmail.com>
2021-08-23 23:36:04 +0100

    create all necessary paths

---
+++ src/components/components/Form/FormControl/index.js
@@ -0,0 +1,6 @@
+export { default as NameInput } from "./NameInput";
+export { default as SurnameInput } from "./SurnameInput";
+export { default as Email } from "./Email";
+export { default as Textarea } from "./Textarea";
+export { default as RadioInput } from "./RadioInput";
+export { default as Checkbox } from "./Checkbox";
