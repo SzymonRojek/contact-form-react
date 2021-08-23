51a9f95b4a4d1d910eef83004303290c8c809763
Szymon Rojek <sz.rojek@gmail.com>
2021-08-23 23:35:51 +0100

    create Form functionality and component with all FormControls inputs

---
+++ src/components/components/Form/Form.js
@@ -0,0 +1,85 @@
+import { useForm } from "react-hook-form";
+
+import {
+  NameInput,
+  SurnameInput,
+  Email,
+  Textarea,
+  RadioInput,
+  Checkbox,
+} from "./FormControl";
+
+const styledError = {
+  message: {
+    color: "crimson",
+    fontSize: "14px",
+  },
+  input: { border: "1px solid crimson" },
+};
+
+const Form = ({ setIsSubmitted }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = () => {
+    setIsSubmitted(true);
+    reset();
+  };
+
+  return (
+    <>
+      <p className="paragraph-required">*All fields required</p>
+      <form onSubmit={handleSubmit(onSubmit)} className="form">
+        <NameInput
+          register={register}
+          errors={errors}
+          styledError={styledError}
+        />
+
+        <SurnameInput
+          register={register}
+          errors={errors}
+          styledError={styledError}
+        />
+
+        <Email register={register} errors={errors} styledError={styledError} />
+
+        <Textarea
+          register={register}
+          errors={errors}
+          styledError={styledError}
+        />
+
+        <div className="form-control-wrapper">
+          <div>
+            <p className="form-paragraph">Geneder options:</p>
+
+            <RadioInput register={register} value="female" label="female" />
+
+            <RadioInput register={register} value="male" label="male" />
+
+            <RadioInput register={register} value="other" label="other" />
+
+            {errors.gender && (
+              <p style={styledError.message}>{errors.gender.message}</p>
+            )}
+          </div>
+        </div>
+
+        <Checkbox
+          register={register}
+          errors={errors}
+          styledError={styledError}
+        />
+
+        <button className="form-button">send</button>
+      </form>
+    </>
+  );
+};
+
+export default Form;
